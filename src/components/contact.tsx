6d794538b1ab73ecd5b291a830418878b9c0f735
phuopham <108047552+phuopham@users.noreply.github.com>
2023-10-10 10:32:26 +0700

    update footer and title

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,23 +1,25 @@
 import { useSectionInView } from '../libs/hooks';
+import { motion } from 'framer-motion'
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const Contact = () => {
-    const { ref } = useSectionInView("Experience");
+    const { ref } = useSectionInView("Experience", 0.5);
     return (
-        <section id='contact' className='bg-purple-200 dark:bg-purple-950 w-full mt-28 px-4 py-14' ref={ref}>
+        <motion.section
+            id='contact' className='bg-purple-200 dark:bg-purple-950 w-full mt-28 px-4 py-14' ref={ref}>
             <div className='max-w-[45rem] m-auto'>
                 <h2 className="text-3xl font-medium mb-8 text-center">Contact me</h2>
                 <p className='text-center'>
                     Whether you have a question or just want to say hi, my inbox is always open and i'll try my best to get back to you. You could drop me an email at{" "}
                     <a href="mailto:thanhphuong89@gmail.com" className='underline hover:text-gray-500'>thanhphuong89@gmail.com</a> or send me message via following channels:
                 </p>
-                <p className='flex justify-center gap-2 mt-4'>
-                    <a href="https://github.com/phuopham"><BsGithub className="w-6 h-6" /></a>
-                    <a href="https://www.linkedin.com/in/phuong89/"><BsLinkedin className="w-6 h-6" /></a>
-                    <a href="https://phuopham.github.io/resume/">My resume</a>
+                <p className='flex justify-center gap-6 mt-4 items-center'>
+                    <a className='hover:scale-125' href="https://github.com/phuopham"><BsGithub className="w-8 h-8" /></a>
+                    <a className='hover:scale-125' href="https://www.linkedin.com/in/phuong89/"><BsLinkedin className="w-8 h-8" /></a>
+                    <a className='hover:scale-125 text-bold bg-gray-50 rounded-full px-2 py-1 font-semibold text-purple-950' href="https://phuopham.github.io/resume/">My resume</a>
                 </p>
             </div>
-        </section>
+        </motion.section>
     )
 }
 
