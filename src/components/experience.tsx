440ddac1f688ebf171f70ded53d59a54c8ae6cb3
phuopham <108047552+phuopham@users.noreply.github.com>
2023-10-25 14:40:36 +0700

    add portfolio proj to projects

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -16,21 +16,21 @@ export default function Experience({ experiences }: { experiences: Array<Experie
       className="max-w-[45rem] text-center leading-8 mt-28 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
+      transition={{ delay: 0.75 }}
       id="experience"
     >
       <h2 className="m-auto py-3 px-12 rounded-full inline-block text-3xl font-medium text-purple-700 dark:mix-blend-difference bg-slate-100/10 mb-8 text-center">My experience</h2>
-      <ol className="border-l-2 border-opacity-70 border-neutral-300 dark:border-neutral-500">
+      <ol className="border-l-2 border-opacity-70 border-slate-300/75 dark:border-slate-500/75">
         {experiences?.map((it, key) => (
           <li key={key}>
             <div className="flex-start flex items-center pt-3">
-              <p className="-ml-[5px] p-2 pe-4 bg-gray-300 dark:bg-gray-500 rounded-s-xl rounded-e-full text-sm">
+              <p className="-ml-[5px] p-2 pe-4 bg-slate-300/75 dark:bg-slate-500/75 rounded-s-xl rounded-e-full text-sm">
                 {it.from}
               </p>
             </div>
-            <div className="mb-6 ml-4 mt-2">
+            <div className="mb-5 ml-2 mt-1 px-5 py-2 rounded-xl bg-slate-50/75 dark:bg-slate-50/10">
               <div className='flex justify-between'>
-                <h4 className="mb-1.5 text-purple-500 text-xl font-semibold">
+                <h4 className="mb-1.5 dark:mix-blend-difference text-purple-500 text-xl font-semibold">
                   {it.title}
                 </h4>
                 <h4 className="mb-1.5 text-md font-bold">
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -11,13 +11,35 @@ export type ProjectsDataProps = {
   repo?: string
 }
 
-export default function Projects({ projectsData }: { projectsData: Array<ProjectsDataProps> | undefined }) {
 
+export default function Projects({ projectsData }: { projectsData: Array<ProjectsDataProps> | undefined }) {
+  let allcatalogs
+  if (projectsData)
+    allcatalogs = projectsData.reduce((acc, obj) => {
+      return acc.concat(obj.tags);
+    }, [] as string[]);
+
+
+  const [catalogs, setCatalogs] = React.useState([...new Set(allcatalogs)].map(item => ({ value: item, selected: true })))
   const { ref } = useSectionInView("Projects", 0.5);
+
+  const onSelectionChange = (e: string) => {
+    setCatalogs(prevItems =>
+      prevItems.map(item =>
+        item.value === e ? { ...item, selected: false } : item
+      )
+    )
+    console.log(catalogs)
+  }
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mt-28 flex flex-col items-center">
       <h2 className="m-auto py-3 px-12 rounded-full text-3xl font-medium text-purple-700 dark:mix-blend-difference bg-slate-100/10 mb-8 text-center">My projects</h2>
+      <div className="flex gap-2 flex-wrap pb-8">
+        {catalogs.map((it, key) => (
+          <button key={key} className="rounded-full px-3 py-1 bg-slate-100/30 border-purple-800 border-[1px] opacity-70 transition-all hover:bg-slate-100 text-sm" value={it.value} onClick={(e) => onSelectionChange((e.target as HTMLButtonElement).value)} >{it.value}</button>
+        ))}
+      </div>
       <div>
         {projectsData?.map((project, index) => (
           <React.Fragment key={index}>
