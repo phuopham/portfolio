c653669f8b74d1f1486d1a22eecd7708f3082c0c
phuopham <108047552+phuopham@users.noreply.github.com>
2023-12-21 16:38:46 +0700

    fix mobile responsive issue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,7 +37,7 @@ export const Header = () => {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[5.15rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-2 sm:h-[3.95rem] sm:w-[36rem] sm:rounded-3xl dark:bg-gray-50 dark:border-black/40 dark:bg-opacity-75 text-center text-purple-600 font-bold dark:text-purple-600"
+        className="fixed top-0 left-1/2 h-[5.15rem] w-[100vw] rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-2 sm:h-[3.95rem] sm:w-[36rem] sm:rounded-3xl dark:bg-gray-50 dark:border-black/40 dark:bg-opacity-75 text-center text-purple-600 font-bold dark:text-purple-600"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       >Phuong Pham</motion.div>
